81884d96715fcc11f0d85bec1857ed1cbf042ba2
cyran <gs.gouin@gmail.com>
2023-01-05 18:54:45 +0100

    header et composant mis

---
+++ src/app/coiffeur/coiffeur.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coiffeur',
+  templateUrl: './coiffeur.component.html',
+  styleUrls: ['./coiffeur.component.scss']
+})
+export class CoiffeurComponent {
+
+}
