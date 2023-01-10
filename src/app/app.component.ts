de1f96a87bc43e04b63673e5c8add8e203bd8259
cyran <gs.gouin@gmail.com>
2023-01-10 18:09:17 +0100

    fonction creer user ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,11 @@ import {UserService} from "./services/user.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(private afs: AngularFirestore, private userService: UserService) {
   }
 
- myCollection: Subscription | undefined;
+  myCollection: Subscription | undefined;
   // tutorial: AngularFirestoreDocument<any>;
   title = 'AndreStella';
   idUi: number[] = [];
@@ -23,58 +23,55 @@ export class AppComponent implements OnInit{
 
 
   ngOnInit() {
-    this.retrieveUsers()
-   // this.tutorial = this.afs.doc('tutorial')
-   //  const collectionUsers = this.afs.collection('users');
-   //  const user = {title: 'blalvla', url:'http'}
-    // collectionUsers.add({...user})
-
-    // this.myCollection = this.afs.collection('lulu').valueChanges().subscribe(v => console.log(v));
-    // this.afs.collection('users').doc('Nou5DkL8ofVanDCNpCGB').set({toto: 'titi'})
-    // this.afs.collection('users').doc('tutorial').set({ae: 'aaa'})
+    this.retrieveUsers();
   }
-retrieveUsers(){
+  createUSer(listUser: User[]){
+    listUser.map(user => {
+      user.id = this.generatorIdentifiant();
+      // this.userService.create(user).then(() => console.log('user creer'))
+    })
+  }
+  retrieveUsers() {
     this.userService.getAll().snapshotChanges().pipe(
-      map(changes =>
-      changes.map(c => ({id: c.payload.doc.id, ...c.payload.doc.data() })
-        )
-      )
-  ).subscribe(data => {
-      this.userList = data
-      console.log(this.userList)
+      map(changes => changes.map(c => ({id: c.payload.doc.id, ...c.payload.doc.data()}))))
+        .subscribe(data => {
+        this.userList = data
+        // console.log('list in firebase ',this.userList)
     });
-}
-  generatorIdentifiant(): void{
+  }
+
+  generatorIdentifiant(): number {
     const id = Math.floor(Math.random() * (999999 - 111111) + 111111);
-    if(id !== 99999 && id !== 111111 && !this.idUi.includes(id)) this.idUi.push(id);
+    if (id !== 99999 && id !== 111111 && !this.idUi.includes(id)) this.idUi.push(id)
     else this.generatorIdentifiant();
+    return id
   }
 
   readFile($event: Event) {
     const inputEl = $event.currentTarget as HTMLInputElement;
     let fileList: FileList | null = inputEl.files;
     if (fileList?.length) {
-      console.log(fileList)
       const fileReader = new FileReader();
       fileReader.onload = () => {
-        let text = fileReader.result as string;
-        const tab = text.split('\n').map(ligne => ligne.toLowerCase().split(',').map(colonne => colonne.trim()));
-        console.log(tab)
-        const tabUSer: Partial<User>[] = [];
-        tab.map(info => {
-          console.log(info)
-          const user: Partial<User> = {name:info[0], username: info[1], tel:info[2], mail:info[3]}
-          tabUSer.push(user);
-        })
-        console.log(tabUSer)
-        const re = tabUSer.map(user =>{
-          const userString = JSON.stringify(user, (key, value) => value === undefined ? null : value);
-          return JSON.parse(userString);
-        })
-        console.log(re)
+        const text = fileReader.result as string;
+        const tabUser = this.formatText(text);
+        this.createUSer(tabUser);
       }
       fileReader.readAsText(fileList[0])
     }
+  }
+  formatText(text: string): Partial<User>[] {
+      return text
+      .split('\n')
+      .filter(ligne => ligne)
+      .map(ligne => ligne.toLowerCase())
+      .map(ligne => ligne.split(',').map(colonne => colonne.trim()))
+      .map(x => ({name: x[0]? x[0]: '', username: x[1]? x[1]: '', tel: x[2]? x[2]: '' , mail: x[3]? x[3]: ''}));
+
+    // const re = tabUSer.map(user => {
+    //   const userString = JSON.stringify(user, (key, value) => value === undefined ? null : value);
+    //   return JSON.parse(userString);
+    // })
   }
 }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {map} from "rxjs";
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(private userService: UserService) {
+  }
+  userList?: any[];
+  form: FormGroup = new FormGroup({
+    numero: new FormControl('', [Validators.required])
+  });
 
+  ngOnInit() {
+   this.retrieveUsers();
+  }
+  retrieveUsers() {
+    this.userService.getAll().snapshotChanges().pipe(
+      map(changes => changes.map(c => ({id: c.payload.doc.id, ...c.payload.doc.data()}))))
+      .subscribe(data => {
+        this.userList = data
+        console.log('list in firebase ',this.userList)
+      },
+          error => console.log('erreur :', error),
+        () => console.log('tache fini')
+      );
+  }
+  submit(){
+    const isNumeroPresent = this.userList?.map(x => x.id).includes(Number(this.form.value.numero));
+    console.log(isNumeroPresent)
+  }
 }
