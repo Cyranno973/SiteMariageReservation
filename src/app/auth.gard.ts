f1a60e52822d9c404fd745b97cc538ec2eb99449
cyran <gs.gouin@gmail.com>
2023-02-17 19:25:10 +0100

    navigation admin et protection des routes en place et amelioré

--- src/app/auth.gard.ts
+++ src/app/auth.gard.ts
@@ -1,33 +1,29 @@
-import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
-import {map, Observable} from "rxjs";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {combineLatest, map, Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {StoreUserService} from "./services/store-user.service";
+
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGard implements CanActivate{
-  constructor(private storeUserService: StoreUserService, private router: Router, private ac:ActivatedRoute) {
-  }
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-    if(state.url === '/admin'){
-      return this.storeUserService.observeIsAdmin().pipe(map(bool => {
-          if(bool) return true
-          else {
-            this.router.navigate(['/home']);
-            return false
-          }
-        })
-      )
-    }else{
-      return this.storeUserService.observeUser().pipe(map(user => {
-          if(user) return true
-          else {
-            this.router.navigate(['/home']);
-            return false
-          }
-        })
-      )
-    }
+export class AuthGard implements CanActivate {
+  constructor(private storeUserService: StoreUserService, private router: Router) {
   }
 
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    const subAdmin = this.storeUserService.observeIsAdmin()
+    const subUser = this.storeUserService.observeUser()
+    return combineLatest([subAdmin, subUser]).pipe(map(x => {
+      console.log(x)
+      if (x[0]) {
+        console.log('admin activé')
+        return true
+      } else if (x[1] && state.url !== '/admin') {
+        return true
+      } else {
+        this.router.navigate(['/home']);
+        return false
+      }
+    }))
+  }
 }
