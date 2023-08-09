1153b972e2afe1f5ff53bc77cb8f2297b96bda0a
cyran <gs.gouin@gmail.com>
2023-08-09 02:20:42 +0200

    version prod 1

--- src/app/services/assets-data.service.ts
+++ src/app/services/assets-data.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
 import {map, Observable} from "rxjs";
-import { Media } from "../../model/media"
-import {User} from "../../model/user";
+import {Media} from "../../model/media"
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,19 +14,22 @@ export class AssetsDataService {
   constructor(private db: AngularFirestore) {
     this.mediaRef = db.collection(this.dbPath);
   }
+
   getAll(): Observable<any> {
     return this.mediaRef.snapshotChanges().pipe(
       map(changes => changes.map(c => ({...c.payload.doc.data(), id: c.payload.doc.id}))));
   }
+
   createOrUpdate(media: Media, creation: boolean = false): any {
     return this.mediaRef.doc(media.id).set(media).then(asset => console.log('asset creer'));
   }
+
   updateOrderInFirestore(activityList: Media[]): Promise<void> {
     const batch = this.db.firestore.batch();
 
     activityList.forEach((item, index) => {
       const docRef = this.db.collection(this.dbPath).doc(item.id).ref;
-      batch.update(docRef, { order: index });
+      batch.update(docRef, {order: index});
     });
 
     return batch.commit().then(() => {
@@ -35,9 +38,11 @@ export class AssetsDataService {
       console.error('Error updating order:', error);
     });
   }
+
   delete(id: string): Promise<void> {
     return this.mediaRef.doc(id).delete()
   }
+
   update(media: Partial<Media>): any {
     return this.mediaRef.doc(media.id).update(media).then(media => console.log('media modifier'))
   }
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from "@angular/core";
+import {Component, Input, OnInit, ViewEncapsulation} from "@angular/core";
 import {FormGroup} from "@angular/forms";
 import {Menu} from "../../model/user";
 
@@ -8,34 +8,21 @@ import {Menu} from "../../model/user";
   styleUrls: ['./user-form.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class UserFormComponent implements OnInit{
+export class UserFormComponent implements OnInit {
   @Input() form: FormGroup;
   @Input() index: number;
-  // @Output() formChange: EventEmitter<any> = new EventEmitter();
   dishes: string[] = [];
   categories = ['Adulte', 'Enfant'];
-  ngOnInit() {
-   if(this.form.get('menu')?.value) {
-     this.populateMenus();
-   }
 
-  }
-
-  populateMenus(){
-    this.dishes = this.form.get('selectedCategory')?.value === 'Enfant'? [Menu.Child] : [Menu.Fish, Menu.Meat];
+  ngOnInit()  {
+    if (this.form.get('menu')?.value) this.populateMenus();
   }
 
   onCategoryChange() {
-    console.log('ttaa')
     this.populateMenus();
+    const menu = this.form.get('selectedCategory')?.value === 'Enfant' ? Menu.Child : '';
+    this.form.get('menu')?.setValue(menu);
+  }
 
-    if (this.form.get('selectedCategory')?.value === 'Enfant') {
-      this.form.patchValue({
-        menu: Menu.Child
-      });
-    } else {
-      this.form.patchValue({
-        menu: ''
-      });
-    }}
+  private populateMenus = () => this.dishes = this.form.get('selectedCategory')?.value === 'Enfant' ? [Menu.Child] : [Menu.Fish, Menu.Meat];
 }
